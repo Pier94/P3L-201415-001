c0655c4849572a5a2f229fb9e41e5b9fdef28c13
Pier94 <pierpaolo.sepe@studenti.uniparthenope.it>
2014-11-28 09:57:16 +0100

    Added some methods

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 public class Main {
 
+  public Main(String args []) {
+
+ 
+ }
+  
+  public void run() {
+   
+    System.out.println("Hello world");
+ 
+  }
+
   public static void main(String args[]) {
   
     System.out.println("Questo è il main");
 
-  
+    Main main = new Main(args);
+    main.run();
   }
 
 
